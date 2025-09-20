6ff8bcc89fa29a29d2aed7d5510c99dfc12db713
mtmatt <mtmattqq@gmail.com>
2025-09-20 23:03:12 +0800

    feat: add some language support

--- backend/src/utils/language-support.ts
+++ backend/src/utils/language-support.ts
@@ -1,30 +1,34 @@
 export default {
-  'gcc c11': {
-    compileCommand: 'gcc -std=c11 main.c -o main',
+  'gcc c17': {
+    compileCommand: 'gcc -std=c17 main.c -o main',
     executeCommand: './main.exe'
   },
-  'g++ c++14': {
-    compileCommand: 'g++ -std=c++14 main.cpp -o main',
+  'gcc c23': {
+    compileCommand: 'gcc -std=c23 main.c -o main',
     executeCommand: './main.exe'
   },
   'g++ c++17': {
     compileCommand: 'g++ -std=c++17 main.cpp -o main',
     executeCommand: './main.exe'
   },
+  'g++ c++23': {
+    compileCommand: 'g++ -std=c++23 main.cpp -o main',
+    executeCommand: './main.exe'
+  },
   rust: {
     compileCommand: 'rustc main.rs -o main',
     executeCommand: './main.exe'
   },
-  // 'nodejs': {
-  //     compileCommand: '',
-  //     executeCommand: 'node main.js',
-  // },
-  // 'python3': {
-  //     compileCommand: '',
-  //     executeCommand: 'python3 main.py',
-  // },
-  // 'bash': {
-  //     compileCommand: '',
-  //     executeCommand: 'bash main.sh',
-  // },
+  'nodejs': {
+    compileCommand: '',
+    executeCommand: 'node main.js',
+  },
+  'python3': {
+    compileCommand: '',
+    executeCommand: 'python3 main.py',
+  },
+  'bash': {
+    compileCommand: '',
+    executeCommand: 'bash main.sh',
+  },
 };
