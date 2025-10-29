1f40b986fa905fc8537998d75c22ed14e29e2b6c
mtmatt <mtmattqq@gmail.com>
2025-10-30 01:09:57 +0800

    fix: typo

--- backend/__test__/api-test/src/main.rs
+++ backend/__test__/api-test/src/main.rs
@@ -2,6 +2,6 @@ use api_test::user_api_tests::random_user_api_calls;
 
 #[tokio::main]
 async fn main() {
-    // Run the random request workload (10k requests) and print throughput.
+    // Run the random request workload (32k requests) and print throughput.
     random_user_api_calls(32_000).await;
 }
